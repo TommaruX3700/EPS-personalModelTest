ab7c8008af79cc6826c3f4b6b06426c3f06ee839
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-09 13:38:12 +0100

    fixed code format

--- main.cpp
+++ main.cpp
@@ -12,10 +12,11 @@
     #include "headers/logic/sceltaPacchiNesting.hpp"
     #include "headers/logic/spostaPacchi.hpp"
     #include "headers/logic/trovaDominio.hpp"
-
 #pragma endregion
 
-int consoleErrorMessage (std::string errorString);
+#pragma region "Function declarations"
+    int consoleErrorMessage (std::string errorString);
+#pragma endregion
 
 int main (int argc, char* argv[]) 
 {
@@ -24,30 +25,20 @@ int main (int argc, char* argv[])
         #pragma region "Startup Routine"
             //Gets arguments as INPUT
             const std::string inputJsonPath = argv[0];
-            
-            std::cout 
-            << "#######################" << "\n" 
-            << "EPS-MODEL: START" << "\n" 
-            << "#######################" << "\n" 
-            << "Input argument: " << inputJsonPath 
-            << std::endl;
+            consoleStartMessage(inputJsonPath);
 
             //Check if file exists 
             std::ifstream inputJsonFile;
-
             inputJsonFile.open(inputJsonPath);
             if (!inputJsonFile)
             {
-                //file doesnt exists: throw error
+                //File doesnt exists: throw error
                 throw std::invalid_argument("Invalid JSON path provided or unable to access file.");
             }
-            
 
             //Extract packs from it
 
-
             //TODO: add input control
-
 
         #pragma endregion
 
@@ -58,27 +49,37 @@ int main (int argc, char* argv[])
         
         return 0;
     }
-
     catch(const std::invalid_argument& e){
         return consoleErrorMessage(e.what());
     }
-
     catch(const std::exception& e)
     {
         return consoleErrorMessage(e.what());
     }   
 }
 
-int consoleErrorMessage (std::string errorString) 
-{
+#pragma region "Console messages"
+    void consoleStartMessage (std::string inputString)
+    {
         std::cout 
         << "#######################" << "\n" 
-        << "EPS-MODEL: ERROR" 
-        << "#######################" << "\n" <<
-        std::endl;
-        
-        std::cerr
-        << errorString << '\n'
-        << "#######################" << "\n";
-        return -1;
-}
+        << "EPS-MODEL: START" << "\n" 
+        << "#######################" << "\n" 
+        << "Input argument: " << inputString 
+        << std::endl;
+    }
+
+    int consoleErrorMessage (std::string errorString) 
+    {
+            std::cout 
+            << "#######################" << "\n" 
+            << "EPS-MODEL: ERROR" 
+            << "#######################" << "\n" <<
+            std::endl;
+            
+            std::cerr
+            << errorString << '\n'
+            << "#######################" << "\n";
+            return -1;
+    }
+#pragma endregion
