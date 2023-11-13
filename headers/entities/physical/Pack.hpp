248510a7b0a2799c732c207f6ea31a6afd8e7c7e
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-13 15:20:07 +0100

    Pack.hpp

--- headers/entities/physical/Pack.hpp
+++ headers/entities/physical/Pack.hpp
@@ -15,15 +15,28 @@ private:
     bool palletizable;
     
 public:
+#pragma region "Constructors"
     Pack();
     Pack(ThreeD_coords<float> coords, float WEIGHT, int CODE, bool ROT) {};
     ~Pack();
+#pragma endregion
 
+#pragma region "Operators override"
     Pack &operator=(const Pack &n);
     Pack &operator==(const Pack &n);
+#pragma endregion
 
+#pragma region "Set methods"
     void setRotatable(bool ROT);
     void setPackID(int CODE);
-    
+#pragma endregion    
+
+#pragma region "Get methods"
     int getPackID();
+#pragma endregion
+
+#pragma region "Methods"
+
+#pragma endregion
+
 };
