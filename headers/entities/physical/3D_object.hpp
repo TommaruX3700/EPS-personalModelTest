b89e28568519a207ccc75479087f3d72a1a0435a
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-13 14:33:59 +0100

    3D_object.hpp

--- headers/entities/physical/3D_object.hpp
+++ headers/entities/physical/3D_object.hpp
@@ -1,1 +1,36 @@
 #include "../geometry/Geometry.hpp"
+
+class ThreeD_object
+{
+private:
+    
+public:
+    ThreeD_coords<float> objectDims;
+    ThreeD_coords<float> oldObject_Dims;
+    ThreeD_coords<float> center;
+    float weight;
+    float density;
+
+    ThreeD_object();
+    ~ThreeD_object();
+
+    void setDims(float x, float y, float z);
+    void setCenterCoords(ThreeD_coords<float> objectDims); //TODO: verify argument type
+    void setWeight(float kg);
+    void setOrientation(int orientation);
+
+    ThreeD_coords<float> getDims();
+    float getVolume();
+    float getDensity();
+    
+};
+
+ThreeD_object::ThreeD_object()
+{
+
+}
+
+ThreeD_object::~ThreeD_object()
+{
+
+}
