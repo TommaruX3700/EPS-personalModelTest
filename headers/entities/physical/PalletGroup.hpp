8eea78a569adee883a79096e13da4efc425adc8c
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-13 17:00:41 +0100

    PalletGroup.hpp

--- headers/entities/physical/PalletGroup.hpp
+++ headers/entities/physical/PalletGroup.hpp
@@ -1,1 +1,22 @@
 #include "Pallet.hpp"
+#include <vector>
+
+/*
+Author:
+    Tommaso Maruzzo
+Description:
+    PalletGroup made upon a vector of pointers to Pallet objects.
+*/
+
+typedef Pallet* ptrPallet;
+
+class PalletGroup
+{
+private:
+    std::vector<ptrPallet> palletGroup;
+    int max = 0; 
+    
+public:
+    PalletGroup();
+    ~PalletGroup();
+};
