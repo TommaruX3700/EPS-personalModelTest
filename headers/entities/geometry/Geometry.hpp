6e200b4875cf022e9cdb9b23bfe41fc62c6086f4
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-13 17:28:46 +0100

    Geometry.hpp fix

--- headers/entities/geometry/Geometry.hpp
+++ headers/entities/geometry/Geometry.hpp
@@ -13,15 +13,15 @@ Description:
 template<typename type>
 struct ThreeNum_set { //type for 3D coordinates
 	type X, Y, Z;
-	ThreeD_coords() : X(type(0.0)), Y(type(0.0)), Z(type(0.0)) {}; //general constructor
-	ThreeD_coords(type x_val, type y_val, type z_val) : X(type(x_val)), Y(type(y_val)), Z(type(z_val)) {}; //specific constructor
+	ThreeNum_set() : X(type(0.0)), Y(type(0.0)), Z(type(0.0)) {}; //general constructor
+	ThreeNum_set(type x_val, type y_val, type z_val) : X(type(x_val)), Y(type(y_val)), Z(type(z_val)) {}; //specific constructor
 };
 
 template<typename type>
 struct TwoNum_set { //type for 2D coordinates
 	type X, Y;
-	TwoD_coords() : X(type(0.0)), Y(type(0.0)) {};
-	TwoD_coords(type x_val, type y_val) : X(type(x_val)), Y(type(y_val)) {};
+	TwoNum_set() : X(type(0.0)), Y(type(0.0)) {};
+	TwoNum_set(type x_val, type y_val) : X(type(x_val)), Y(type(y_val)) {};
 };
 
 #endif
