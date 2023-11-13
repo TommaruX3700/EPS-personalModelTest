61217e3a14cef3f756c843ade502546640a4a369
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-13 15:12:28 +0100

    Pack.hpp

--- headers/entities/physical/Pack.hpp
+++ headers/entities/physical/Pack.hpp
@@ -1,1 +1,30 @@
-#include "../geometry/Geometry.hpp"
+#include "../physical/3D_object.hpp"
+
+/*
+Author:
+    Tommaso Maruzzo
+Description:
+    Pack object.
+*/
+
+class Pack : ThreeD_object
+{
+private:
+    int packID;
+    bool rotatable;
+    bool palletizable;
+    
+public:
+    Pack(/* args */);
+    ~Pack();
+};
+
+Pack::Pack(/* args */)
+{
+}
+
+Pack::~Pack()
+{
+}
+
+
