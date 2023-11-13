c713870f25aa6ff03b9312f0488244e761ab3835
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-13 16:55:48 +0100

    Pallet.hpp

--- headers/entities/physical/Pallet.hpp
+++ headers/entities/physical/Pallet.hpp
@@ -1,2 +1,31 @@
 #include "../geometry/Geometry.hpp"
 #include "Pack.hpp"
+#include <vector>
+
+/*
+Author: 
+    Tommaso Maruzzo
+Description:
+    Pallet object.
+*/
+
+typedef Pack* ptrPack; //pointer to Pack object
+
+class Pallet : ThreeD_object
+{
+private:
+    float param_Efficency;
+    std::vector<ptrPack> pallet;
+
+public:
+    Pallet();
+    Pallet(ThreeD_coords<float> maxDimensions) {};
+    ~Pallet();
+    
+    void addPack(ptrPack packToAddPtr); //IMPLEMENTATION: add Pack pointer to the 
+    void addPack(ptrPack packToAddPtr, int vectorPos = NULL); //IMPLEMENTATION: add Pack pointer to defined position in the pallet vector list
+    void rmvPack(); //IMPLEMENTATION: delete the last one
+    void rmvPack(int vectorPos = -1); //IMPLEMENTATION: default -1 but delete the one in the defined position
+    int packCount();
+
+};
