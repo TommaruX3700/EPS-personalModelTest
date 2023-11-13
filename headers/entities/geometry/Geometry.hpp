e1fe34469741eaab7af3b66994a07dff5b2e649f
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-13 17:22:42 +0100

    Geometry.hpp

--- headers/entities/geometry/Geometry.hpp
+++ headers/entities/geometry/Geometry.hpp
@@ -11,16 +11,16 @@ Description:
 */
 
 template<typename type>
-struct ThreeD_coords { //type for 3D coordinates
+struct ThreeNum_set { //type for 3D coordinates
 	type X, Y, Z;
 	ThreeD_coords() : X(type(0.0)), Y(type(0.0)), Z(type(0.0)) {}; //general constructor
 	ThreeD_coords(type x_val, type y_val, type z_val) : X(type(x_val)), Y(type(y_val)), Z(type(z_val)) {}; //specific constructor
 };
 
 template<typename type>
-struct TwoD_coords { //type for 2D coordinates
+struct TwoNum_set { //type for 2D coordinates
 	type X, Y;
-	TwoD_coords() : X(type(0)), Y(type(0)) {};
+	TwoD_coords() : X(type(0.0)), Y(type(0.0)) {};
 	TwoD_coords(type x_val, type y_val) : X(type(x_val)), Y(type(y_val)) {};
 };
 
