a51879efe5801d541877532c5477c338502e6e4a
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-20 12:57:29 +0100

    Added basic variables on ordinamentoPacchi.hpp

--- headers/logic/ordinamentoPacchi.hpp
+++ headers/logic/ordinamentoPacchi.hpp
@@ -1,4 +1,5 @@
 #include "../entities/physical/Pack.hpp"
+#include <vector>
 
 /*
 Author:
@@ -6,3 +7,7 @@ Author:
 Description:
     Vector-Pack sorting method to handle Pack vectors to compute in all the other nesting operations
 */
+
+std::vector<Pack*> packsToSort; //INPUT
+std::vector<Pack*> sortedPacks; //OUTPUT_1 -> sorted pointers-to-objects vector
+std::vector<Pack*> notSortablePacks; //OUPUT_2 -> packs flagged with "not packable"
