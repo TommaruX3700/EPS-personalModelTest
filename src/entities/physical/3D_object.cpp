bf3906a1cb2b081081fb276f45274a8057fdbfca
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-20 16:47:55 +0100

    Update 3D_object.src

--- src/entities/physical/3D_object.cpp
+++ src/entities/physical/3D_object.cpp
@@ -1,1 +1,11 @@
 #include "../../headers/entities/physical/3D_object.hpp"
+
+ThreeD_object :: ThreeD_object() 
+{
+    //General constructor
+}
+
+ThreeD_object :: ~ThreeD_object() 
+{
+    //General distructor
+}
