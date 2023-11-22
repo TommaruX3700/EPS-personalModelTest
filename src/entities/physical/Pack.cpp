29442bbe3b181d80513a181e008ba417d795c79f
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-22 15:26:08 +0100

    operator= Pack.cpp

--- src/entities/physical/Pack.cpp
+++ src/entities/physical/Pack.cpp
@@ -1,21 +1,33 @@
 #include "../../headers/entities/physical/Pack.hpp"
 
-Pack :: Pack (ThreeNum_set<float> dims, ThreeNum_set<float> coords, float WEIGHT, int CODE, bool ROT) {
+Pack :: Pack (ThreeNum_set<float> dims, ThreeNum_set<float> coords, float WEIGHT, int CODE, bool ROT) 
+{
 
 }
 
-Pack :: ~Pack() {
+Pack :: ~Pack() 
+{
 
 }
 
 #pragma region "Operator overrides"
 //Check if this works correctly as intended (eg: not only exchanging pointers)
-Pack& Pack :: operator=(const Pack& n) {
+
+Pack& Pack :: operator=(const Pack& n) 
+{
+    if (this != &n)
+    {
+        //copy info in new object
+        n.getDims();
+        this->setDims(n.getDims());
+    }
     
 }
 
-Pack& Pack :: operator==(const Pack& n) {
-
+Pack& Pack :: operator==(const Pack& n) 
+{
+    
+    
 }
 
 #pragma endregion
