c98d2f700d5113cb36aaf50c35e09fc34f955ace
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-11-22 13:44:07 +0100

    Update Pack.cpp

--- src/entities/physical/Pack.cpp
+++ src/entities/physical/Pack.cpp
@@ -1,1 +1,9 @@
 #include "../../headers/entities/physical/Pack.hpp"
+
+Pack :: Pack (ThreeNum_set<float> dims, ThreeNum_set<float> coords, float WEIGHT, int CODE, bool ROT) {
+
+}
+
+Pack :: ~Pack() {
+
+}
