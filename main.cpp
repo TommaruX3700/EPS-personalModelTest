45bb043d810419bf05042bf7ce20a770110cb708
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-01-09 23:11:26 +0100

    #25: riordinato il codice

--- main.cpp
+++ main.cpp
@@ -32,69 +32,70 @@ int main (int argc, char* argv[])
 {  
     try
     {
-        consoleStartMessage();
+    consoleStartMessage();
 
     #pragma region "TEST_CODE"
         #if TEST_MODE == 1
 
-            std::string outputTestString = "";
-            /*
-                DOING: Ticket #25
-            */
+        std::string outputTestString = "";
+        /*
+            DOING: Ticket #25
+        */
+
         #pragma region "test Pack & 3D_object"
-                    //PACK: funziona
-                    ThreeNum_set<int> testDims, testCoords;
-                    testDims.X = 4;
-                    testDims.Y = 6;
-                    testDims.Z = 2;
-                    testCoords.X = 5; 
-                    testCoords.Y = 5;
-                    testCoords.Z = 5; 
+            //PACK: funziona
+            ThreeNum_set<int> testDims, testCoords;
+            testDims.X = 4;
+            testDims.Y = 6;
+            testDims.Z = 2;
+            testCoords.X = 5; 
+            testCoords.Y = 5;
+            testCoords.Z = 5; 
 
-                    float testWeight = 2.4;
-                    int testID = 10;
-                    bool testFlag = true;
+            float testWeight = 2.4;
+            int testID = 10;
+            bool testFlag = true;
 
-                    Pack testPack1(testDims, testCoords, testWeight, testID, testFlag);
+            Pack testPack1(testDims, testCoords, testWeight, testID, testFlag);
+
+            testDims.X = 2;
+            testDims.Y = 2;
+            testDims.Z = 2;
+            testCoords.X = 1; 
+            testCoords.Y = 1;
+            testCoords.Z = 1; 
+
+            Pack testPack2(testDims, testCoords, 1, 20, true);
                     
-                    testDims.X = 2;
-                    testDims.Y = 2;
-                    testDims.Z = 2;
-                    testCoords.X = 1; 
-                    testCoords.Y = 1;
-                    testCoords.Z = 1; 
+            ThreeNum_set<int> extractedDims;
+            extractedDims = testPack1.getDims();
+            ThreeNum_set<int> extractedCoords;
+            extractedCoords = testPack1.getCenterCoords();
 
-                    Pack testPack2(testDims, testCoords, 1, 20, true);
+            outputTestString = " Pack1 dims are: \n X -> " + std::to_string(extractedDims.X) + "\n Y -> " + std::to_string(extractedDims.Y) + "\n Z -> " + std::to_string(extractedDims.Z) + "\n";
+            outputTestString += "\n Pack1 coords are: \n X -> " + std::to_string(extractedCoords.X) + "\n Y -> " + std::to_string(extractedCoords.Y) + "\n Z -> " + std::to_string(extractedCoords.Z) + "\n";
+            outputTestString += "\n Pack1 Volume: " + std::to_string(testPack1.getVolume());
+            outputTestString += "\n Pack1 ID: " + std::to_string(testPack1.getPackID());
+            outputTestString += "\n Pack1 Density: " + std::to_string(testPack1.getDensity());
                     
-                    ThreeNum_set<int> extractedDims;
-                    extractedDims = testPack1.getDims();
-                    ThreeNum_set<int> extractedCoords;
-                    extractedCoords = testPack1.getCenterCoords();
+            //testPack2 = testPack1;
 
-                    outputTestString = " Pack1 dims are: \n X -> " + std::to_string(extractedDims.X) + "\n Y -> " + std::to_string(extractedDims.Y) + "\n Z -> " + std::to_string(extractedDims.Z) + "\n";
-                    outputTestString += "\n Pack1 coords are: \n X -> " + std::to_string(extractedCoords.X) + "\n Y -> " + std::to_string(extractedCoords.Y) + "\n Z -> " + std::to_string(extractedCoords.Z) + "\n";
-                    outputTestString += "\n Pack1 Volume: " + std::to_string(testPack1.getVolume());
-                    outputTestString += "\n Pack1 ID: " + std::to_string(testPack1.getPackID());
-                    outputTestString += "\n Pack1 Density: " + std::to_string(testPack1.getDensity());
+            extractedDims = testPack2.getDims();
+            extractedCoords = testPack2.getCenterCoords();
 
-                    //testPack2 = testPack1;
-
-                    extractedDims = testPack2.getDims();
-                    extractedCoords = testPack2.getCenterCoords();
-
-                    outputTestString += "\n\n Pack2 dims are: \n X -> " + std::to_string(extractedDims.X) + "\n Y -> " + std::to_string(extractedDims.Y) + "\n Z -> " + std::to_string(extractedDims.Z) + "\n";
-                    outputTestString += "\n Pack2 coords are: \n X -> " + std::to_string(extractedCoords.X) + "\n Y -> " + std::to_string(extractedCoords.Y) + "\n Z -> " + std::to_string(extractedCoords.Z) + "\n";
-                    outputTestString += "\n Pack2 Volume: " + std::to_string(testPack2.getVolume());
-                    outputTestString += "\n Pack2 ID: " + std::to_string(testPack2.getPackID());
-                    outputTestString += "\n Pack2 Density: " + std::to_string(testPack2.getDensity());
+            outputTestString += "\n\n Pack2 dims are: \n X -> " + std::to_string(extractedDims.X) + "\n Y -> " + std::to_string(extractedDims.Y) + "\n Z -> " + std::to_string(extractedDims.Z) + "\n";
+            outputTestString += "\n Pack2 coords are: \n X -> " + std::to_string(extractedCoords.X) + "\n Y -> " + std::to_string(extractedCoords.Y) + "\n Z -> " + std::to_string(extractedCoords.Z) + "\n";
+            outputTestString += "\n Pack2 Volume: " + std::to_string(testPack2.getVolume());
+            outputTestString += "\n Pack2 ID: " + std::to_string(testPack2.getPackID());
+            outputTestString += "\n Pack2 Density: " + std::to_string(testPack2.getDensity());
                     
-                    if (testPack1 == testPack2)
-                    {
-                        outputTestString += "\n\n I PACCHI SONO UGUALI :)";
-                    } else
-                    {
-                        outputTestString += "\n I PACCHI SONO DIVERSI :(";
-                    }
+            if (testPack1 == testPack2)
+            {
+                outputTestString += "\n\n I PACCHI SONO UGUALI :)";
+            } else
+            {
+                outputTestString += "\n I PACCHI SONO DIVERSI :(";
+            }
         #pragma endregion
 
         #pragma region "test creazione Grid"
@@ -120,13 +121,14 @@ int main (int argc, char* argv[])
             testGrid.freeOccupiedCells(&testPack1);
             testGrid.freeOccupiedCells(&testPack1);
         #pragma endregion
-            std :: cout << "piano1: XY" << std :: endl;
-            testGrid.printGrid(1);
-            std :: cout << "piano2: YZ" << std :: endl;
-            testGrid.printGrid(2);
-            std :: cout << "piano3: XZ" << std :: endl;
-            testGrid.printGrid(3);
-            //consoleLog(outputTestString);
+
+        std :: cout << "piano1: XY" << std :: endl;
+        testGrid.printGrid(1);
+        std :: cout << "piano2: YZ" << std :: endl;
+        testGrid.printGrid(2);
+        std :: cout << "piano3: XZ" << std :: endl;
+        testGrid.printGrid(3);
+        //consoleLog(outputTestString);
 
     #pragma endregion
 
