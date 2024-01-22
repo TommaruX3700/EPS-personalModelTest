3a89ffbbea96f0b4082ef05a8cc78c682190438d
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-01-22 17:47:47 +0100

    #10: porcodio

--- main.cpp
+++ main.cpp
@@ -69,6 +69,7 @@ int main (int argc, char* argv[])
                         ReadJson jsonConverter(jsonStringContent);
                         packVector packs = jsonConverter.getPackVector();
                         Pallet palletInfo(jsonConverter.getPalletInfos());
+
                     }
                 }
                 else
