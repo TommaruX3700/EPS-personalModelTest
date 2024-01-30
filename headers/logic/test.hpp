a3eb14fd08fb75b0c89932fd0a1590a670a1ca5e
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-01-30 20:30:22 +0100

    #29: - fixed test.hpp (removed unused includes - project builds fine

--- headers/logic/test.hpp
+++ headers/logic/test.hpp
@@ -7,19 +7,10 @@
     #include <algorithm>
     #include <regex>
 
-    #include "../../extLibs/nlohmannJSON/json.hpp"
-    #include "../../extLibs/rapidjson/document.h"
-
     #include "../entities/physical/Pack.hpp"
     #include "../entities/physical/PalletGroup.hpp"
     #include "../entities/geometry/Grid.hpp"
 
-    #include "letturaFileJson.hpp" 
-    #include "ordinamentoPacchi.hpp"
-    #include "ottimizzazione.hpp"
-    #include "sceltaPacchiNesting.hpp"
-    #include "spostaPacchi.hpp"
-    #include "trovaDominio.hpp"
 #pragma endregion
 
 #pragma region  "Test functions"
