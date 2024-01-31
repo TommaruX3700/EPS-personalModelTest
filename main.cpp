aa48f7fb966bfdbb92f9fdcb1097f8fc13646ab8
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-01-31 10:31:34 +0100

    #29: - moved all messages & logs methods to external implementation files

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
     #include "headers/entities/physical/PalletGroup.hpp"
     #include "headers/entities/geometry/Grid.hpp"
 
+    #include "headers/logic/messages.hpp" 
     #include "headers/logic/letturaFileJson.hpp" 
     #include "headers/logic/ordinamentoPacchi.hpp"
     #include "headers/logic/ottimizzazione.hpp"
@@ -26,10 +27,6 @@
 #pragma endregion
 
 #pragma region "Function declarations"
-    int consoleErrorMessage(std::string errorString);
-    void consoleStartMessage();
-    void consoleLog(std::string message);
-    void testFunction();
     //json vectorToJson (all the arguments needed);
 
 #pragma endregion
@@ -53,7 +50,7 @@ int main (int argc, char* argv[])
                 {
                     //Gets arguments as INPUT
                     inputJsonPath = argv[1];
-                    consoleStartMessage();
+                    consoleStartMessage(inputJsonPath);
 
                     //Check if file exists 
                     std :: ifstream inputFile(inputJsonPath);
@@ -226,6 +223,8 @@ int main (int argc, char* argv[])
 
 #pragma region "Function implementations"
 
+    //TODO: move most of this in external files
+
     //TODO:
     //  - [ ] Ticket: "main: End Routine - vectorToJson() implementation"
     //
@@ -234,40 +233,5 @@ int main (int argc, char* argv[])
     //      Build the output json, structure defined in Documentation.
     //      Examples&Wiki: https://json.nlohmann.me/api/json/
     //}
-
-    void consoleLog (std::string message)
-    {
-        #if TEST_MODE == 1
-            std::cout << "EPS-M_TEST_MODE: " << "\n";
-        #else
-            std::cout << "EPS-LogMessage: " << "\n\n"; 
-        #endif
-            std ::cout << message << "\n" << "-------------------------------------------------" << std::endl;
-    }
-
-    void consoleStartMessage ()
-    {
-        #if TEST_MODE == 1
-            std::cout << "\nEPS-M_TEST_MODE: START" << "\n";
-            std ::cout << "-------------------------------------------------" << std::endl;
-
-        #else
-            std::cout << "\nEPS-M: START" << "\n";
-            std::cout << "> Input: " << inputJsonPath << "\n" << "-------------------------------------------------" << std::endl;
-
-        #endif
-    }
-
-    int consoleErrorMessage (std::string errorString) 
-    {
-        #if TEST_MODE == 1
-            std::cout << "EPS-M_TEST_MODE: ERROR" << "\n" << "> ";
-        #else
-            std::cout << "EPS-M: ERROR" << "\n" << "> ";
-        #endif
-            std::cerr << errorString << '\n';
-            std::cout << "-------------------------------------------------" << "\n" << std::endl;
-        return -1;
-    }
     
 #pragma endregion
