b97e6724f1cac56506a9f413ee3da90782808311
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-03-10 15:59:39 +0100

    Updated nesting() logic

--- src/logic/BoxNesting/BoxNesting.cpp
+++ src/logic/BoxNesting/BoxNesting.cpp
@@ -22,26 +22,26 @@ BoxNesting :: ~BoxNesting()
 
 void BoxNesting :: nesting()
 {
-    // TODO: capire che DS implementare x il Dominio di piazzamento. 
-    // magari una classe, con metodi per aggiungere automaticamente e sommare i domini, dando come input un oggetto pacco.
-    // + metodi tutti per interagire con suddetto dominio.
-
+    /*
+    *   Nesting loop, cycling until there are any elements left inside inputPacks
+    */
     while (this->inputPacks.size())
     {
         Pack nextPack;
         try
         {
-            nextPack = *this->inputPacks.front();   // Using only "front" pack grants me more freedom if I have to iterate itself again with other ops, afther optimization
-
-#pragma region "AlG1: CodeBlock 2.3.1"
             /*
-            *   FOCUS:
-            *       - trovare nuovo dominio di piazzamento 
-            *   
+            *   Using only "front" pack grants me freedom on iterating a single Pack element again, usually after a domain optimization
+            */
+            nextPack = *this->inputPacks.front();   
+            
+            /*  
             *   TODO: 
             *       - aggiungere variabile Dominio_Totale anche se non so quanto utile sia, dato che va ricalcolato ad ogni aggiunta di pacco.
             *       - aggiungere variabile per "ottimizzazione" nel calcolo Dominio_Totale (identificare come fare)
-            *   
+            */
+
+            /*   
             *   OPERATIONS: 
             *       if(is primo pacco nel pallet)
             *           {
@@ -55,12 +55,11 @@ void BoxNesting :: nesting()
             *                       if(firstPlacement()) 
             *                           { 
             *                               //fatto questo, ho generato la mia configurazione ROOT di partenza, che va ora però ottimizzata MUOVENDO i pacchi
-            *                               > trovo il min(Ntot), ovvero trovo il valore VERO più vicino al minimo, per cui TUTTI i percorsi che iterconnettono i centri dei pacchi tra loro è il minimo possibile, rispettando le dimensioni dei pacchi, baricentro, limiti del pallet 
             *                               if(nestingForMinN())
             *                                   {
             *                                       // il minimo esiste
             *                                       // pacchi già spostati e nextPack piazzato
-            *                                       // elimina nextPack dalla stack "inputPacks"
+            *                                       deleteFrontPack(); 
             *                                   }
             *                               else 
             *                                   {
@@ -70,35 +69,44 @@ void BoxNesting :: nesting()
             *                       else
             *                           {
             *                               addToNotPalletizable(pacco); 
-            *                               // elimina nextPack dalla stack "inputPacks"
+            *                               deleteFrontPack(); 
             *                           }
             *                               
             *                   } 
             *               else
             *                   {
             *                       addToNotPalletizable(pacco);
-            *                       // elimina nextPack dalla stack "inputPacks"
+            *                       deleteFrontPack(); 
             *                   }
             *           }
-            *       
-            *   OTHER_FUNCTIONS:
-            *       > void placeAtCenter(pacco);________________________________________________________________________________//piazzo al centro del pallet
-            *       > bool findDomain(pallet.packVector, pacco_in_entrata, old_DominioTotale, optimization, &Dominio_Totale);___//trovo nuovo dominio di piazzamento rispetto alla configurazione attuale, rispettando anche i domini esclusi da "optimization"
-            *       > startingPlacement(pacco, (&Dominio_Totale));______________________________________________________________//funzione che piazza il pacco in un punto del piano ammesso dal Dominio_totale
-            *       > addToNotPalletizable(pacco);______________________________________________________________________________//aggiungo pacco a vettore di pacchi non palletizzabili
             */
-#pragma endregion
 
+            /*
+            *   OTHER_FUNCTIONS:
+            *       - void placeAtCenter(pacco);
+            *               > piazzo al centro del pallet"
+            * 
+            *       - bool findDomain(pallet.packVector, pacco_in_entrata, old_DominioTotale, optimization, &Dominio_Totale);
+            *               > trovo nuovo dominio di piazzamento rispetto alla configurazione attuale, rispettando anche i domini esclusi da "optimization"
+            * 
+            *       - startingPlacement(pacco, (&Dominio_Totale));
+            *               > funzione che piazza il pacco in un punto del piano ammesso dal Dominio_totale
+            * 
+            *       - addToNotPalletizable(pacco);
+            *               > aggiungo pacco a vettore di pacchi non palletizzabili
+            * 
+            *       - nestingForMinN();
+            *               > trovo il min(Ntot), ovvero trovo il valore VERO più vicino al minimo, per cui TUTTI i percorsi che iterconnettono i centri dei pacchi tra loro è il minimo possibile, rispettando le dimensioni dei pacchi, baricentro, limiti del pallet 
+            * 
+            *       - deleteFrontPack(); 
+            *               > elimina nextPack dalla stack "inputPacks"
+            */
         }
         catch(const std::exception& e)
         {
             std::cerr << "Pacco non piazzabile:" << e.what() << '\n';
             this->outputNotNestedPacks->push_back(&nextPack);
         }
-        
-        /*
-        *   ALG_2: CodeBlock 2.3.2
-        */
     }
 
     /*
