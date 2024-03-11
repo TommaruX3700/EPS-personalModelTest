6fb1c5cff71e111e164acb2fc9e6100e8b397fb5
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-03-11 18:42:37 +0100

    Updated work

--- headers/entities/geometry/PackDomain.hpp
+++ headers/entities/geometry/PackDomain.hpp
@@ -21,6 +21,14 @@ class PackDomain
 };
 /*
 *   How should I rappresent an interval of points into a XY graph, in C++?
+*   IDEA:
+*       Copy the idea of Plane class method "findDimsRoutine()" and get then a TwoNum_set of integers, rapresenting both the intervals
+*   Every "Interval" will be defined by: 
+*       {
+*           int height1  //how many is the interval "detatched" by its parallel axis
+*           int height2  //height1-pack/
+*           TwoNum_set interval //effective interval
+*       }
 */
 
 // I need to rapresent make a Datastructure for a "Domain" wich is described by 4 different ints. I then want to make another DataStructure "DomainTotal", made of multiple instances of the "Domain". "DomainTotal" should be 
