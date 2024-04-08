cb0503e6f633edc6b8d147aba6b7b73bfb5fff44
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-04-08 22:33:50 +0200

    asd

--- src/logic/BoxNesting/mainLoops.cpp
+++ src/logic/BoxNesting/mainLoops.cpp
@@ -1,9 +1,9 @@
 #include "../../../headers/logic/BoxNesting/mainLoops.hpp"
+#include "../../../headers/logic/OrdinamentoPacchi/quickSort.hpp"
+
 
 void MainNestLoops(dims palletDims, palletVector* outPalletVector, packVector inPacksToNest, packVector* outUnNestablePacks)
 {
-
-#pragma region "LOOP_1"
     /*
      *   Questo loop ad ogni ciclo ogni pacco contenuto nel buffer dei pacchi scartati.
      *   > packs.first -> pacchi su cui lanciare il thread di nesting
@@ -21,21 +21,39 @@ void MainNestLoops(dims palletDims, palletVector* outPalletVector, packVector in
         Pallet* newPallet = new Pallet(palletDims);
         
         outPalletVector->push_back(*newPallet);
+
         /*
          *   Execute a sortInput and reorder the packs.
          */
-        tempPairVector = sortInput(tempPairVector.second, *newPallet);
+        output output;
+        float area = 0;
+        quickSort(tempPairVector.second, 0, tempPairVector.second.size()-1);
+
+        if ((tempPairVector.second[0]->getDims().num1*tempPairVector.second[0]->getDims().num2) >= (newPallet->getPalletDims().num1*newPallet->getPalletDims().num2))
+        {
+            
+        } 
+        else 
+        {
+            for(const auto& pack : tempPairVector.second){
+                area+=(pack->getDims().num1*pack->getDims().num2);
+                if (area <= (newPallet->getPalletDims().num1*newPallet->getPalletDims().num2))
+                    outPalletVector->push_back(pack);
+                else
+                    output.second.push_back(pack);
+            }
+        }
+
         /*
          *   Define an operation object and pass new parameters to it.
          */
         packVector toNest, notNested;
         toNest = tempPairVector.first;
+
         /*
          *   Move un-Nested Packs to the second part of the pair to sortInput them in the next phase.
          */
     } while (tempPairVector.second.size());
-
-#pragma endregion
 
 // Gest out in the referenced Pack vector the unNested Packs
     *outUnNestablePacks = tempPairVector.second;
--- src/logic/ordinamentoPacchi/ordinamentoPacchi.cpp
+++ src/logic/ordinamentoPacchi/ordinamentoPacchi.cpp
@@ -2,10 +2,10 @@
 #include "../../../headers/logic/OrdinamentoPacchi/quickSort.hpp"
 
 /*
-*   Gives as output a pair of Pack* vectors, based on their flag:
-*   > output.first = palletizable packs;
-*   > output.second = nonPalletizable packs.
-*/
+ *   Gives as output a pair of Pack* vectors, based on their flag:
+ *   > output.first = palletizable packs;
+ *   > output.second = nonPalletizable packs.
+ */
 output isPalletizable(packVector inputVector)
 {
     output output;
@@ -19,16 +19,21 @@ output isPalletizable(packVector inputVector)
 }
 
 /*
-*   Gives as output a pair of Pack* vectors, based upon their dimensions and pallet area (if they will fit or not, matematically)
-*   > output.first =  nestable packs stack;
-*   > output.second = packs to re-iterate to find a nestable stack;
-*/
+ *   Gives as output a pair of Pack* vectors, based upon their dimensions and pallet area (if they will fit or not, matematically)
+ *   > output.first =  nestable packs stack;
+ *   > output.second = packs to re-iterate to find a nestable stack;
+ */
 output sortInput(packVector inputVector, Pallet pallet)
 {
-    /*TODO mettere in modo che venga sortato anche in base al peso massimo*/
+    /*
+     * TODO:
+     *  - mettere in modo che venga sortato anche in base al peso massimo
+     *  - se il primo pacco è più grande dell'area, perde di senso e area sarà sempre più grande: devo includere un vettore di output per i pacchi non palletizzabili o con area più grande del pallet stesso.
+     */
     output output;
     float area = 0;
     quickSort(inputVector, 0, inputVector.size()-1);
+    
     for(const auto& pack : inputVector){
         area+=(pack->getDims().num1*pack->getDims().num2);
         if (area <= (pallet.getPalletDims().num1*pallet.getPalletDims().num2))
