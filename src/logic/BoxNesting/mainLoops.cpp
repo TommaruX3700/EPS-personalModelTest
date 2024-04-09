94ef27d1bc8ace995bf56cd7dfba7c72a1ca3eec
TommaruX3700 <tommasomaruzzo@gmail.com>
2024-04-09 15:46:28 +0200

    asd

--- src/logic/BoxNesting/mainLoops.cpp
+++ src/logic/BoxNesting/mainLoops.cpp
@@ -10,6 +10,7 @@ void MainNestLoops(dims palletDims, palletVector* outPalletVector, packVector in
      *   > packs.second -> pacchi "scartati" dal nesting e che necessitano di tornare sotto elaborazione fino ad un tot massimo.
      */
     std::pair<packVector, packVector> tempPairVector;
+    
     tempPairVector.first.clear();
     tempPairVector.second.clear();
     tempPairVector.second = inPacksToNest;
@@ -19,42 +20,41 @@ void MainNestLoops(dims palletDims, palletVector* outPalletVector, packVector in
          *   Add pallet vector reference to save reference.
          */
         Pallet* newPallet = new Pallet(palletDims);
-        
         outPalletVector->push_back(*newPallet);
 
         /*
          *   Execute a sortInput and reorder the packs.
          */
-        output output;
         float area = 0;
+        float pack_area = 0;
+        float pallet_area = newPallet->getPalletDims().num1*newPallet->getPalletDims().num2;
         quickSort(tempPairVector.second, 0, tempPairVector.second.size()-1);
 
-        if ((tempPairVector.second[0]->getDims().num1*tempPairVector.second[0]->getDims().num2) >= (newPallet->getPalletDims().num1*newPallet->getPalletDims().num2))
+        if ((tempPairVector.second[0]->getDims().num1*tempPairVector.second[0]->getDims().num2) >= pallet_area)
         {
-            
+            //do this or skip pack or put in another list
+            newPallet->addPack(*tempPairVector.second[0]);
+            tempPairVector.second.erase(tempPairVector.second.begin());
         } 
         else 
         {
+            int cursor = 0;
             for(const auto& pack : tempPairVector.second){
                 area+=(pack->getDims().num1*pack->getDims().num2);
-                if (area <= (newPallet->getPalletDims().num1*newPallet->getPalletDims().num2))
-                    outPalletVector->push_back(pack);
+                if (area <= pallet_area)
+                {
+                    newPallet->addPack(*pack);
+                    tempPairVector.second.erase(tempPairVector.second.begin() + cursor);
+                    //remove the pack to the tempPairVector
+                    // probabilemnte il valore di cursors fa casino
+                    cursor++;
+                }
                 else
-                    output.second.push_back(pack);
+                    break;
             }
         }
-
-        /*
-         *   Define an operation object and pass new parameters to it.
-         */
-        packVector toNest, notNested;
-        toNest = tempPairVector.first;
-
-        /*
-         *   Move un-Nested Packs to the second part of the pair to sortInput them in the next phase.
-         */
+    /*
+     *   Move un-Nested Packs to the second part of the pair to sortInput them in the next phase.
+     */
     } while (tempPairVector.second.size());
-
-// Gest out in the referenced Pack vector the unNested Packs
-    *outUnNestablePacks = tempPairVector.second;
 }
